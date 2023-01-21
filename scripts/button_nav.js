d86b03e2baeae3c78c3e173f4fad9ae3baf961f0
NomenNescio1 <u1201416@unimilitar.edu.co>
2023-01-20 21:37:10 -0500

    fix(): ajuste sintaxis

--- scripts/button_nav.js
+++ scripts/button_nav.js
@@ -1,41 +1,41 @@
-function loadContent (file,section) {
+function loadContent(file, section) {
   let sectionToUse = "container-section1";
-    // (A) FETCH "DUMMY.HTML"
-    fetch("data/"+file+".html")
-    
+  // (A) FETCH "DUMMY.HTML"
+  fetch(`data/${file}.html`)
     // (B) RETURN THE RESULT AS TEXT
     .then((result) => {
-      if (result.status != 200) { throw new Error("Bad Server Response"); }
+      if (result.status !== 200) {
+        throw new Error("Bad Server Response")
+      }
       return result.text();
     })
-   
+
     // (C) PUT LOADED CONTENT INTO <DIV>
     .then((content) => {
-      if (section == 1) { 
+      if (section == 1) {
         eraseSection2();
-      }else{
+      } else {
         sectionToUse = "container-section2";
       }
       document.getElementById(sectionToUse).innerHTML = content;
     })
-   
+
     // (D) HANDLE ERRORS
     .catch((error) => { console.log(error); });
 }
 
-function eraseSection2(){
+function eraseSection2() {
   fetch("data/empty.html")
-  .then((result) => {
-    if (result.status != 200) { throw new Error("Bad Server Response"); }
-    return result.text();
-  })
-  .then((content) => {
-    document.getElementById("container-section2").innerHTML = content;
-  })
-  .catch((error) => { console.log(error); });
+    .then((result) => {
+      if (result.status != 200) { throw new Error("Bad Server Response"); }
+      return result.text();
+    })
+    .then((content) => {
+      document.getElementById("container-section2").innerHTML = content;
+    })
+    .catch((error) => { console.log(error); });
 }
 
-window.onload = function() {
-  loadContent ('projects',1);
+window.onload = function () {
+  loadContent('projects', 1);
 };
-
